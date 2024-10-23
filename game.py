2839e1b22046ec5572d3fcc1a05cbe53caf0b3f9
Walteir Coelho <wcfreitasf@gmail.com>
2024-10-23 20:09:48 -0300

    Implementado o método Play

--- game.py
+++ game.py
@@ -4,8 +4,29 @@ def main() -> None:
     points: int = 0
     play(points)
 
-def play(point: int) -> None:
-    pass
+def play(points: int) -> None:
+    difficulty: int = int(input('Informe o nível de dificuldade desejado[1, 2, 3, 4]: '))
+
+    calc: Calculate = Calculate(difficulty)
+
+    print('Informe o resultado para a seguinte operção: ')
+    calc.show_operation()
+
+    result: int = int(input())
+
+    if calc.result_check(result):
+        points += 1
+        print(f'Você tem {points} ponto(s).')
+
+    continues: int = int(input('Deseja continuar no jogo? [1 - sim, 0 - nao] '))
+
+    if continues:
+        play(points)
+    else:
+        print(f'Vcoê finalizou com {points} ponto(s).')
+        print('Até a próxima!')
+
+
 
 
 if __name__ == '__main__':
